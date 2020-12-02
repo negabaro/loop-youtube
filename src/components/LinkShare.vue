a2dd4dc6817bb746de8d97c9e56a4da6fb7b3ac7
negabaro <negabaro@naver.com>
2020-12-02 23:34:29 +0900

    add sharebutton

--- src/components/LinkShare.vue
+++ src/components/LinkShare.vue
@@ -8,7 +8,9 @@
       class="button search-button"
       data-clipboard-target="#qr-url"
     >Link Copy</button>
+
     <h2 class="nav-link subtitle is-size-3">
+
       <!-- <a href="https://www.instagram.com/romesrf/"><span class="icon"><svg
             aria-hidden="true"
             focusable="false"
@@ -25,32 +27,36 @@
               class=""
             ></path>
           </svg></span></a> -->
-      <span class="icon is-small">
-        <i class="fab fa-youtube svg-inline--fa"></i>
-      </span>
+      <a
+        href="#"
+        @click="onClickTwitterShareButton"
+        target="_blank"
+        alt="Share on Twitter"
+      >
+        <span class="icon is-small">
+          <i class="fab fa-twitter svg-inline--fa"></i>
+        </span>
+
+      </a>
 
       <a
         href="#"
-        onclick="javascript:window.open('https://twitter.com/intent/tweet?text=[%EA%B3%B5%EC%9C%A0]%20' +encodeURIComponent(document.URL)+'%20-%20'+encodeURIComponent(document.title), 'twittersharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;"
+        @click="onClickFacebookShareButton"
         target="_blank"
-        alt="Share on Twitter"
-      ><span class="icon"><svg
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fab"
-            data-icon="twitter"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            class="svg-inline--fa fa-twitter fa-w-16"
-          >
-            <path
-              fill="currentColor"
-              d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
-              class=""
-            ></path>
-          </svg></span></a>
-      <a href="https://facebook.com/loop-youtube"><span class="icon">
+        alt="Share on Facebook"
+      >
+        <!-- <span class="icon">
+          <img
+            alt="Vue logo"
+            src="../assets/icon/iconmonstr-facebook-6.svg"
+          />
+        </span> -->
+
+        <span class="icon is-small">
+          <i class="fab fa-facebook svg-inline--fa"></i>
+        </span>
+      </a>
+      <!-- <a href="https://facebook.com/loop-youtube"><span class="icon">
           <svg
             aria-hidden="true"
             focusable="false"
@@ -81,7 +87,7 @@
               d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
               class=""
             ></path>
-          </svg></span></a>
+          </svg></span></a> -->
     </h2>
   </div>
 </template>
@@ -90,9 +96,20 @@
 import Vue from "vue";
 import ClipboardJS from "clipboard";
 import { toHHMMSS } from "@/util/index";
+///Users/sehwakim/docker/loop-youtube/src/assets/icon/facebook.svg
+//import facebook from "../assets"
+//import Facebook from "../assets/icon/facebook.svg";
+
+//import Facebook2 from "~/assets/icon/iconmonstr-facebook-6.svg";
+
+//import Facebook from "./facebook.svg";
+//import DownloadIcon from '@/assets/images/icons/download.svg?component'
 
 export default Vue.extend({
   name: "searchButton",
+  components: {
+    //Facebook2
+  },
   data() {
     return {
       value: ""
@@ -136,6 +153,28 @@ export default Vue.extend({
     //}
   },
   methods: {
+    onClickTwitterShareButton() {
+      window.open(
+        "https://twitter.com/intent/tweet?text=[%EA%B3%B5%EC%9C%A0]%20" +
+          encodeURIComponent(this.shareLinkUrl()) +
+          "%20-%20" +
+          encodeURIComponent(document.title),
+        "twittersharedialog",
+        "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600"
+      );
+      return false;
+    },
+    onClickFacebookShareButton() {
+      window.open(
+        "https://www.facebook.com/sharer/sharer.php?u=" +
+          encodeURIComponent(this.shareLinkUrl()) +
+          "&t=" +
+          encodeURIComponent(document.title),
+        "facebooksharedialog",
+        "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600"
+      );
+    },
+
     onClick() {
       const clipboard = new ClipboardJS("#copy-button");
     },

83fa9c8b01cf61e528ba37bdc27805ff94313b70
negabaro <negabaro@naver.com>
2020-12-02 23:34:13 +0900

    scss 기반추가 +  add fontawesome

--- src/components/navLink.vue
+++ src/components/navLink.vue
@@ -16,11 +16,33 @@
               class=""
             ></path>
           </svg></span></a> -->
-    <span class="icon is-small">
-      <i class="fab fa-youtube svg-inline--fa"></i>
-    </span>
+    <a href="https://www.instagram.com/loop-youtube/">
+      <span class="icon is-small">
+        <i class="fab fa-instagram svg-inline--fa"></i>
+      </span>
+    </a>
+    <a href="https://www.youtube.com/loop-youtube/">
+      <span class="icon is-small">
+        <i class="fab fa-youtube svg-inline--fa"></i>
+      </span>
+    </a>
+    <a href="https://www.facebook.com/loop-youtube/">
+      <span class="icon is-small">
+        <i class="fab fa-facebook svg-inline--fa"></i>
+      </span>
+    </a>
+    <a href="https://www.twitter.com/loop-youtube/">
+      <span class="icon is-small">
+        <i class="fab fa-twitter svg-inline--fa"></i>
+      </span>
+    </a>
+    <a href="https://github.com/negabar/loop-youtube">
+      <span class="icon is-small">
+        <i class="fab fa-github svg-inline--fa"></i>
+      </span>
+    </a>
 
-    <a href="https://twitter.com/loop-youtube"><span class="icon"><svg
+    <!-- <a href="https://twitter.com/loop-youtube"><span class="icon"><svg
           aria-hidden="true"
           focusable="false"
           data-prefix="fab"
@@ -67,7 +89,7 @@
             d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
             class=""
           ></path>
-        </svg></span></a>
+        </svg></span></a> -->
   </h2>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,21 @@ import App from "./App.vue";
 //import router from "./router";
 import store from "./store";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+//import { library } from "@fortawesome/fontawesome-svg-core";
+//import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 //import { faYoutube } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+//import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faCoffee);
+require("@/assets/sass/main.scss");
 
-Vue.component("font-awesome-icon", FontAwesomeIcon);
+import "@fortawesome/fontawesome-free/js/fontawesome";
+//import "@fortawesome/fontawesome-free/js/solid";
+//import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
+
+//library.add(faCoffee);
+
+//Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
